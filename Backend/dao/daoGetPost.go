0d324829ade32f598910861fc8b6e057e5fa1f04
wr2233 <wakuda02@g.ecc.u-tokyo.ac.jp>
2024-06-09 21:20:56 +0900

    aa

--- Backend/dao/daoGetPost.go
+++ Backend/dao/daoGetPost.go
@@ -17,7 +17,7 @@ func GetPost(post_id int) (*model.Post, error) {
 
 	// 結果セットから投稿をスキャンして取得する
 	var post model.Post
-	err = row.Scan(&post.Post_id, &post.Content, &post.PostedAt, &post.User_id, &post.Edited, &post.deleted)
+	err = row.Scan(&post.PostID, &post.Content, &post.PostedAt, &post.UserID, &post.Edited, &post.Deleted)
 	if err != nil {
 		return nil, err
 	}
--- Backend/model/postmodel.go
+++ Backend/model/postmodel.go
@@ -3,20 +3,20 @@ package model
 import "time"
 
 type Post struct {
-	Post_id  int       `json:"id"`
-	Content  string    `json:"content"`
-	PostedAt time.Time `json:"postedAt"`
-	User_id  int       `json:"userID"`
-	Edited   bool      `json:"edited"`
-	deleted  bool      `json:"deleted"`
+	PostID   int       `json:"PostID"`
+	Content  string    `json:"Content"`
+	PostedAt time.Time `json:"PostedAt"`
+	UserID   int       `json:"UserID"`
+	Edited   bool      `json:"Edited"`
+	Deleted  bool      `json:"Deleted"`
 }
 
-type reply struct {
-	reply_id    int
-	content     string
-	postedAt    time.Time
-	postedBy_id int
-	postedTo_id int
-	edited      int
-	deleted     int
+type Reply struct {
+	ReplyID    int       `json:"ReplayID"`
+	Content    string    `json:"Content"`
+	PostedAt   time.Time `json:"PostedAt"`
+	PostedByID int       `json:"PostedByID"`
+	PostedToID int       `json:"PostedToID"`
+	Edited     int       `json:"Edited"`
+	Deleted    int       `json:"Deleted"`
 }
