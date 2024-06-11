a7fb5036af95f4961ced0aecc088d74f39a32ba5
wr2233 <wakuda02@g.ecc.u-tokyo.ac.jp>
2024-06-11 23:34:39 +0900

    tyousei tyuu

--- Backend/controller/controllerFollowHandler.go
+++ Backend/controller/controllerFollowHandler.go
@@ -3,12 +3,14 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/WR2233/Hackathon/Backend/dao"
+	"github.com/WR2233/Hackathon/Backend/model"
 	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 )
 
 func followHandler(w http.ResponseWriter, r *http.Request) {
-	var req FollowRequest
+	var req model.FollowRequest
 
 	// リクエストボディをデコード
 	err := json.NewDecoder(r.Body).Decode(&req)
@@ -19,7 +21,7 @@ func followHandler(w http.ResponseWriter, r *http.Request) {
 
 	// フォローまたはアンフォローの処理
 	if req.Num == 0 {
-		err = followUser(req.FollowedByID, req.FollowedToID)
+		err = dao.FollowUser(req.FollowedByID, req.FollowedToID)
 		//} else if req.Num == 1 {
 		//err = unfollowUser(req.FollowedByID, req.FollowedToID)
 	} else {
